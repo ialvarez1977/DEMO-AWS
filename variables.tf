309261cc48055f289d227e392ee49dead94a8749
Ivan Alvarez <ivan.alvarez@hashicorp.com>
2024-05-22 15:34:18 +0200

    CambioVariables

--- variables.tf
+++ variables.tf
@@ -2,17 +2,17 @@ variable "bucketn" {
     default = "bucket-prueba-Monica"
 }
 
-variable "aws_access_key" {
+/*variable "aws_access_key" {
   description = "AWS Access Key"
   default = "bucket-prueba-Monica"
   type        = string
 }
 
-variable "aws_secret_key" {
+#variable "aws_secret_key" {
   description = "AWS Secret Key"
   type        = string
 }
-
+*/
 variable "region" {
   description = "AWS Region"
   type        = string
